71b0d88eca084c4e9105b75d52b70f6d7ded0bb9
Ilo Tech <ilo.nikatsadze@gmail.com>
2026-07-18 00:44:42 +0400

    routing logic changed

--- client/src/pages/HomeContent/Images.jsx
+++ client/src/pages/HomeContent/Images.jsx
@@ -1,23 +1,26 @@
-import { NavLink } from 'react-router-dom'
-import { useContext } from 'react'
-import userContext from '../../assets/context'
-import { URL } from '../../assets/URL'
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import userContext from "../../context/context";
 
 const Images = () => {
-  const {pictures}=useContext(userContext)
-  return (
-    pictures.length?<section className="image-container">
-            {
-                pictures.map((picture,ind)=>{
-                    return(<div key={ind}>
-                        <img src={`${URL}${picture.img_url}`} alt="media image" />
-                        <span>genre : {picture.genre}</span>
-                        <span><NavLink to={`/${picture.img_url}`}>view image</NavLink></span>
-                    </div>)
-                })
-            }
-    </section>:<h1 className="empty-message">No image of this genre so far</h1>
-  )
-}
+  const { pictures } = useContext(userContext);
+  return pictures.length ? (
+    <section className="image-container">
+      {pictures.map((picture, ind) => {
+        return (
+          <div key={picture._id}>
+            <img src={`${picture.img_url}`} alt="media image" />
+            <span>genre : {picture.genre}</span>
+            <span>
+              <NavLink to={`${picture._id}`}>view image</NavLink>
+            </span>
+          </div>
+        );
+      })}
+    </section>
+  ) : (
+    <h1 className="empty-message">No image so far</h1>
+  );
+};
 
-export default Images
+export default Images;
