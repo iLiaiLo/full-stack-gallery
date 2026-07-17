d7356f01f2d2a9efa28bedb07f22ac6dc9fec170
Ilo Tech <ilo.nikatsadze@gmail.com>
2026-07-18 00:56:29 +0400

    future added to BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,24 @@
-import { BrowserRouter,Routes,Route} from 'react-router-dom'
-import userContext from './assets/context'
-import Home from './pages/Home'
-import UploadImage from './pages/UploadImage'
-import Image from './pages/Image'
-import './App.css'
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import userContext from "./assets/context";
+import Home from "./pages/Home";
+import UploadImage from "./pages/UploadImage";
+import Image from "./pages/Image";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
-  const [pictures,setPictures]=useState([])
+  const [pictures, setPictures] = useState([]);
   return (
-    <userContext.Provider value={{pictures,setPictures}}>
-    <BrowserRouter>
-    <Routes>
-      <Route index element={<Home />}/>
-      <Route path={"uploadImage"} element={<UploadImage />} />
-      <Route path={":imageUrl"} element={<Image />} />
-    </Routes>
-    </BrowserRouter>
+    <userContext.Provider value={{ pictures, setPictures }}>
+      <BrowserRouter future={{ v7_startTransition: true }}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path={"uploadImage"} element={<UploadImage />} />
+          <Route path={":id"} element={<Image />} />
+        </Routes>
+      </BrowserRouter>
     </userContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
