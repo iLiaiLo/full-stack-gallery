0bc0fa4b81197a329125a1f091ecf725523cd512
Ilo Tech <ilo.nikatsadze@gmail.com>
2026-07-18 00:42:05 +0400

    controller created

--- client/src/pages/genres/SelectGenres.jsx
+++ client/src/pages/genres/SelectGenres.jsx
@@ -1,15 +1,21 @@
-import genres from "./genres"
+import genres from "./genres";
 
-const SelectGenres = ({setGenre}) => {
+const SelectGenres = ({ setGenre }) => {
+  const handleGenreChange = (e) => {
+    const genre = e.target.value;
+    setGenre(genre);
+  };
   return (
-    <select onChange={(e)=>setGenre(e.target.value)} >
-      {
-        genres.map((genre,ind)=>{
-          return <option key={ind} value={genre}>{genre}</option>
-        })
-      }
-      </select>
-  )
-}
+    <select onChange={handleGenreChange}>
+      {genres.map((genre, ind) => {
+        return (
+          <option key={ind} value={genre}>
+            {genre}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
 
-export default SelectGenres
+export default SelectGenres;
