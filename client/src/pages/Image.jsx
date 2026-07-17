96e51470f1132d6d4ddae0b2048a40266f040642
Ilo Tech <ilo.nikatsadze@gmail.com>
2026-07-18 00:51:19 +0400

    env variable used for api endpoint

--- client/src/pages/Image.jsx
+++ client/src/pages/Image.jsx
@@ -1,62 +1,62 @@
-import { useState,useEffect} from "react";
-import { URL } from "../assets/URL";
-import { useParams,NavLink } from "react-router-dom"
+import { useState, useEffect } from "react";
+// import { URL } from "../assets/URL";
+import { useParams, NavLink } from "react-router-dom";
+
+const URL = import.meta.env.VITE_URL;
 const Image = () => {
-  
-  const {imageUrl}=useParams();
-  const [imageData,setImageData]=useState({})
+  const { id } = useParams();
+  const [imageData, setImageData] = useState({});
 
-
-  useEffect(()=>{
-    async function getData(){
+  useEffect(() => {
+    async function getData() {
       try {
-        const res=await fetch("http://localhost:8000/display_images");
-        const Data=await res.json();
-        const singleImage=Data.find(image=>image.img_url===imageUrl)
-                
-        setImageData(singleImage)
+        const res = await fetch(`${URL}/display_images/${id}`);
+        const singleImage = await res.json();
+
+        setImageData(singleImage);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
-    getData()
+    getData();
+  }, []);
 
-  },[])
-
-  const handleDownload=async (image_url)=>{
-
+  const handleDownload = async (image_url) => {
     try {
-      const response=await fetch(image_url);
-      const blob=await response.blob();
+      const response = await fetch(image_url);
+      const blob = await response.blob();
 
-      const url =window.URL.createObjectURL(blob);
+      const url = window.URL.createObjectURL(blob);
 
-      const a = document.createElement('a');
+      const a = document.createElement("a");
       a.href = url;
 
-      const dotIndex=image_url.lastIndexOf(".");
-      const extname=image_url.slice(dotIndex);
+      const dotIndex = image_url.lastIndexOf(".");
+      const extname = image_url.slice(dotIndex);
 
-      a.download=`${Date.now()}_image${extname}`
+      a.download = `${Date.now()}_image${extname}`;
 
       document.body.appendChild(a);
       a.click();
       a.remove();
       window.URL.revokeObjectURL(url);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  
-  }
+  };
 
   return (
     <div className="single-image">
-      <img src={`${URL}${imageData.img_url}`} alt="media image" />
+      <img src={`${imageData.img_url}`} alt="media image" />
       <span>genre : {imageData.genre}</span>
-      <span><NavLink to={`/`}>back to home page</NavLink></span>
-      <button onClick={()=>handleDownload(`${URL}${imageData.img_url}`)}>download image</button>
+      <span>
+        <NavLink to={"/"}>back to home page</NavLink>
+      </span>
+      <button onClick={() => handleDownload(`${imageData.img_url}`)}>
+        download image
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Image
+export default Image;
